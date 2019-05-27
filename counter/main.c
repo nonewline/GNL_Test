f0f1001bdbea6ea0099005b67f5ded56533c1be8
Ziaul-Haque MAHOMED <zmahomed@c4r21s1.wethinkcode.co.za>
2019-05-27 09:44:51 +0200

    YEETUS

---
+++ counter/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int ft_counter();
+
+int main(void)
+{
+	printf("%d", ft_counter());
+	printf("%d", ft_counter());
+	printf("%d", ft_counter());
+	return (0);
+}
